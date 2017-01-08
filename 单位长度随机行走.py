0dad25b10ab60359334c6821f7a8f434b57723cc
computationalphysics-N2014301020090 <1067327144@qq.com>
2017-01-08 22:33:53 +0800

    Add files via upload

---
+++ 单位长度随机行走.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pylab as pl
+import random
+import math 
+
+class rwalk():
+    """
+    simulating one-dimensional random walks and calculating the mean-squared displacement
+    """
+    def __init__(self,steps):
+        self.steps=steps
+        self.numbers=[]
+        self.x=[0]
+        self.t=[0]
+    def simulate(self):
+        for i in range(int(self.steps)):
+            temp1=random.uniform(0,1)
+            self.t.append(self.t[i]+1)
+            if temp1<=0.5:
+                self.x.append(self.x[i]-1)
+            if temp1>0.5:
+                self.x.append(self.x[i]+1)
+            
+
+        
+        return [self.t,self.x]     
+        
+a1=rwalk(10000)
+a2=rwalk(10000)
+b1=a1.simulate()
+b2=a2.simulate()
+
+pl.plot(b1[0],b1[1],".")
+pl.plot(b2[0],b2[1],".")
+pl.xlabel("step number")
+pl.ylabel("x")
+pl.title("random walk in one dimension")
+
+pl.show()
